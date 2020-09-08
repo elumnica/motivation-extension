a1f1926c602e8defd55370c33f574e33f33ba804
Elvis Lumnica <58716184+elumnica@users.noreply.github.com>
2020-09-08 00:36:29 -0400

    added ajax call to api

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 const contents = document.getElementById('contents');
 contents.parentNode.removeChild(contents);
+
+
+$.ajax({
+    method: 'GET',
+    url: 'http://demo.com',
+    success: function(result) {
+     // result is whatever the URL sends back from the request
+    },
+    error: function(err) {
+     // if any errors occur during the process you can check out the
+     // the error by logging the 'err' argument
+    }
+  };
